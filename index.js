e8ea7ea0cbd4bc98bdc304410bad1ba20e244c0a
aji <a.ji@daum.net>
2017-04-15 11:29:37 +0900

    es6 bug...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(cssText){
-  const style = document.createElement('style');
+  var style = document.createElement('style');
   style.type = 'text/css';
   style.innerHTML = cssText;
   document.getElementsByTagName('head')[0].appendChild(style);
